0b905647461b090e6c77edad32bd0f85cd49d4d1
AnnieEm <annie.eaf@hotmail.com>
2017-05-24 17:39:19 +0100

    Não faz nada!

--- Nhamalicious/Nhamalicious/Resources/Resource.Designer.cs
+++ Nhamalicious/Nhamalicious/Resources/Resource.Designer.cs
@@ -94,26 +94,32 @@ namespace Nhamalicious
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f070008
+			public const int botaoLogin = 2131165192;
+			
 			// aapt resource value: 0x7f070006
-			public const int botaoLogin = 2131165190;
+			public const int botaoSignUp = 2131165190;
 			
 			// aapt resource value: 0x7f070004
-			public const int botaoSignUp = 2131165188;
-			
-			// aapt resource value: 0x7f070002
-			public const int btnLogInReal = 2131165186;
-			
-			// aapt resource value: 0x7f070005
-			public const int textOR = 2131165189;
-			
-			// aapt resource value: 0x7f070001
-			public const int txtPassword = 2131165185;
-			
-			// aapt resource value: 0x7f070003
-			public const int txtRegisto = 2131165187;
+			public const int btnLogInReal = 2131165188;
 			
 			// aapt resource value: 0x7f070000
-			public const int txtUsername = 2131165184;
+			public const int checkedTextView1 = 2131165184;
+			
+			// aapt resource value: 0x7f070002
+			public const int checkedTextView2 = 2131165186;
+			
+			// aapt resource value: 0x7f070007
+			public const int textOR = 2131165191;
+			
+			// aapt resource value: 0x7f070003
+			public const int txtPassword = 2131165187;
+			
+			// aapt resource value: 0x7f070005
+			public const int txtRegisto = 2131165189;
+			
+			// aapt resource value: 0x7f070001
+			public const int txtUsername = 2131165185;
 			
 			static Id()
 			{
