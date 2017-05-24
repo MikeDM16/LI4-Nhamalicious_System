38bb3962f09a1b3b1b812ae47a9d7676e3b2756d
MikeDM16 <miguel_miranda96@live.com.pt>
2017-05-24 23:56:22 +0100

    terminal funfa - Meu branch

--- NhamiBackEnd1/TurnOn.cs
+++ NhamiBackEnd1/TurnOn.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Net.Sockets;
 using System.Windows.Forms;
 using NhamiBackEnd1.Code;
+using NhamiBackEnd1.Code.AcessoBD;
 using System.Text;
 
 namespace NhamiBackEnd1
@@ -18,6 +19,7 @@ namespace NhamiBackEnd1
             InitializeComponent();
             l_displayPort.Text = Server.port.ToString();
             l_displayIP.Text = getMyExternalIP();
+            tb_activity.AppendText(Testar.testarBD());
 
         }
 
