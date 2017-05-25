7f3aeff942678d1b9ac1cab2aed1a068eeb00996
MikeDM16 <miguel_miranda96@live.com.pt>
2017-05-25 15:57:42 +0100

    classes Ingrediente, Preferencia, Cliente e Proprietário

---
+++ NhamiBackEnd1/Code/SharedClasses/Ingrediente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhamiBackEnd1.Code.SharedClasses
+{
+    class Ingrediente
+    {
+        string designacao;
+
+        public Ingrediente(string designacao)
+        {
+            this.designacao = designacao;
+        }
+
+        public string GetDesignacao() { return this.designacao;  }
+        public void SetDesignacao(string d) { this.designacao = d;  }
+    }
+}
