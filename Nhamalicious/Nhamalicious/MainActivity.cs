cf0c7b513ce7463de817d2c61a7d10e8ee1be212
AnnieEm <annie.eaf@hotmail.com>
2017-06-02 23:22:12 +0100

    Registo

--- Nhamalicious/Nhamalicious/MainActivity.cs
+++ Nhamalicious/Nhamalicious/MainActivity.cs
@@ -2,6 +2,8 @@
 using Android.OS;
 using Android.Widget;
 using System;
+using System.Collections.Generic;
+
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
@@ -14,8 +16,9 @@ namespace Nhamalicious
     public class MainActivity : Activity
     {
         private Button mBtnLogin;
+        private Button mBtnRegisto;
         PedidoCli c = new PedidoCli();
-        
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -24,8 +27,9 @@ namespace Nhamalicious
             c.LoginAtempt("John", "123");
 
             mBtnLogin = FindViewById<Button>(Resource.Id.botaoLogin);
+            mBtnRegisto = FindViewById<Button>(Resource.Id.botaoSignUp);
 
-            mBtnLogin.Click += (object sender, EventArgs args) => 
+            mBtnLogin.Click += (object sender, EventArgs args) =>
             {
                 //Pulls up the Dialog Window
                 FragmentTransaction transaction = FragmentManager.BeginTransaction();
@@ -34,6 +38,23 @@ namespace Nhamalicious
                 d1.LoginEfetuado += D1_LoginEfetuado;
             };
 
+
+            mBtnRegisto.Click += (object sender, EventArgs args) =>
+            {
+                //Carrega a página de popup do registo
+                FragmentTransaction transaction = FragmentManager.BeginTransaction();
+                DialogRegistoClass d2 = new DialogRegistoClass();
+                d2.Show(transaction, "dialog fragment");
+                d2.RegistoEfetuado += D2_RegistoEfetuado;
+            };
+        }
+        private void D2_RegistoEfetuado(object sender, OnRegistoEventArgs e)
+        {
+            TcpClient tcp = new TcpClient();
+            tcp.Connect(IPAddress.Parse("192.168.1.92"), 80);
+            // e.Username já é o que o cliente escreve, falta fazer a ligação com o servidor e com os métodos correspondentes
+            // e.Password igual ao username
+            //para os restantes também se faz igual (email, nome...)
         }
 
         private void D1_LoginEfetuado(object sender, OnLoginEventArgs e)
